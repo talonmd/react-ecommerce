5ae3786cd752172b266a0b65bbc5f6b68a6ff6a0
Talon Durrant <talonmdurrant@gmail.com>
2021-01-16 14:52:34 -0700

    implemented firebase utils, included ability to store authenticated userrs into the firebase firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 import Header from "./components/header/header.component"
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 class App extends React.Component {
   constructor() {
@@ -21,10 +21,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        // takes the user information and gets the queryReference
+        // if user doesn't exist, it creates one then still returns the queryReference
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        // takes the snapshot of the reference and sets the state with it
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          })
+          console.log(this.state)
+        })
+      }
+      // on logout, sets the user in state back to null
+      this.setState({ currentUser: userAuth })
     })
   }
 
