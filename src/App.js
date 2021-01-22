d46a657a0ee169360c2c5ef76726606dfb10c7d8
Talon Durrant <talonmdurrant@gmail.com>
2021-01-22 00:34:18 -0700

    using redux saga for the sign in, authentication, and sign out features

--- src/App.js
+++ src/App.js
@@ -11,33 +11,15 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from "./pages/checkout/checkout.component"
 
 import Header from "./components/header/header.component"
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
-import { setCurrentUser } from "./redux/user/user.actions"
 import { selectCurrentUser } from "./redux/user/user.selectors"
+import { checkUserSession } from "./redux/user/user.actions"
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        // takes the user information and gets the queryReference
-        // if user doesn't exist, it creates one then still returns the queryReference
-        const userRef = await createUserProfileDocument(userAuth)
-
-        // takes the snapshot of the reference and sets the state with it
-        userRef.onSnapshot((snapShot) => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data(),
-          })
-        })
-      }
-      // on logout, sets the user in state back to null
-      setCurrentUser(userAuth)
-    })
+    const { checkUserSession } = this.props
+    checkUserSession()
   }
 
   componentWillUnmount() {
@@ -68,7 +50,7 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+  checkUserSession: () => dispatch(checkUserSession()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
